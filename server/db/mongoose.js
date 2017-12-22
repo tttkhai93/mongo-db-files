4593aa2aa93841bbf3611f3bc62ef84dd4129429
Khai <Khai@tttkhai93@gmail.com>
2017-12-21 23:24:41 -0800

    database connecting apps

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+const urls = 'mongodb://tttkhai:123456@ds157653.mlab.com:57653/mgdb';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(urls || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
